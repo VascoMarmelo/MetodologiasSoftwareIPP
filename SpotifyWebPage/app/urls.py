02249f46820bf54929918768320fa3239408dd05
Alexandre Clem <alexandre@educabiz.com>
2024-06-05 19:42:30 -0300

    get the access_token and make api requests

--- SpotifyWebPage/app/urls.py
+++ SpotifyWebPage/app/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
+from django.urls import re_path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='app_index'),
-    path('home_page/', views.index, name='home_page'),
-    path('perfil/', views.app_perfil, name='app_perfil'),
-    path('exemple/', views.app_example, name='app_example'),
+    path('home_page/', views.index, name='home_page'),        
     path('', views.playlist_info, name='playlist_info'),
-    path('example/', views.app_example, name='app_example'),
-    path('perfil/', views.app_profile, name='app_profile')
+    path('example/', views.app_example, name='app_example'),    
+    path('perfil/', views.app_profile, name='app_profile'),
 ]

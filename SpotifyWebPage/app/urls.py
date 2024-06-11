eef9e9abade160c0054d75247d464bfc48c553a8
ColibriLF <24692@ipportalegre.pt>
2024-06-11 19:58:51 +0100

    Playlists + Artists + chatbot

--- SpotifyWebPage/app/urls.py
+++ SpotifyWebPage/app/urls.py
@@ -4,11 +4,18 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='app_index'),
-    path('home_page/', views.index, name='home_page'),        
+    path('home_page/', views.index, name='home_page'),
     #path('', views.playlist_info, name='playlist_info'),
-    path('example/', views.app_example, name='app_example'),    
+    path('example/', views.app_example, name='app_example'),
     path('perfil/', views.app_profile, name='app_profile'),
+    path('infopage/', views.infopage, name='infopage'),
+    path('infopageArt0/', views.infopageArt0, name='infopageArt0'),
+    path('infopageArt1/', views.infopageArt1, name='infopageArt1'),
+    path('infopageArt2/', views.infopageArt2, name='infopageArt2'),
+    path('infopageplay0/', views.infopageplay0, name='infopageplay0'),
+    path('infopageplay1/', views.infopageplay1, name='infopageplay1'),
+    path('infopageplay2/', views.infopageplay2, name='infopageplay2'),
+    path('infopageplay3/', views.infopageplay3, name='infopageplay3'),
+    path('chatboot/', views.chatboot, name='chatboot'),
     path('recommendation/', views.recommendation_view, name='recommendation'),
-    path('artist/', views.app_artist, name='app_artist'),
-    path('track/', views.app_track, name='app_track'),
 ]

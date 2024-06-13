884787608632b3c1b0224d6120cf1a6e554674f5
Alexandre Clem <alexandre@educabiz.com>
2024-06-13 20:57:53 -0300

    Final adjusts

--- SpotifyWebPage/app/urls.py
+++ SpotifyWebPage/app/urls.py
@@ -5,10 +5,18 @@ from . import views
 urlpatterns = [
     path('', views.index, name='app_index'),
     path('home_page/', views.index, name='home_page'),        
-    #path('', views.playlist_info, name='playlist_info'),
+    
     path('example/', views.app_example, name='app_example'),    
     path('perfil/', views.app_profile, name='app_profile'),
-    path('recommendation/', views.recommendation_view, name='recommendation'),
+    
     path('artist/<str:id>/', views.app_artist, name='app_artist'),
     path('track/<str:id>/', views.app_track, name='app_track'),
+    path('recommendation/', views.recommendation_view, name='recommendation'),
+
+    path('chatboot/', views.chatboot, name='chatboot'),
+
+    path('infopageplay0/', views.infopageplay0, name='infopageplay0'),
+    path('infopageplay1/', views.infopageplay1, name='infopageplay1'),
+    path('infopageplay2/', views.infopageplay2, name='infopageplay2'),
+    path('infopageplay3/', views.infopageplay3, name='infopageplay3')    
 ]
